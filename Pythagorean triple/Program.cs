dbaf2aa03d67da727681aaae8894a2310e3449d6
Swati Jain <swati@jain.se>
2018-01-04 13:47:24 +0100

    Add project files.

---
+++ Pythagorean triple/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pythagorean_triple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PythagoreanTriplets PT = new PythagoreanTriplets();
+            int sum = 1000;
+            PT.FindPythagoreanTriplets(sum);
+        }
+    }
+}
---
+++ Pythagorean triple/PythagoreanTriplets.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pythagorean_triple
+{
+    class PythagoreanTriplets
+    {
+        public void FindPythagoreanTriplets(int sum)
+        {
+            for (int a = 1; a <= sum / 3; a++)
+            {
+                for (int b = a + 1; b <= sum / 2; b++)
+                {
+                    int c = sum - a - b;
+                    if (a * a + b * b == c * c)
+                        Console.WriteLine($"a={a}, b={b}, c={c}");
+                }
+            }
+        }
+    }
+}
